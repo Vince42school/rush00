6a4cfda3019c4eda325f4164e16bcdcd9adf409b
Vincent PAILHE <vpailhe@e1r12p7.42.fr>
2015-06-21 06:44:59 +0200

    commit pas marrant

--- login.php
+++ login.php
@@ -6,20 +6,17 @@ $req = mysqli_query($db, $query);
 $row = mysqli_fetch_all($req, MYSQLI_ASSOC);
 foreach ($row as $value)
 {
-	echo "pop corn";
 	if ($_POST['login'] == $value['login'])
 	{
-		echo "NUGETS";
 		if ($pass == $value['password'])
 		{
 			$_SESSION['loggued_on_user'] = $value['login'];
 			$_SESSION['admin'] = $value['admin']; 
-			header("location:display.php");
+			header("Location: display.php");
 			return ;
 		}
-
 	}
 }
 $_SESSION['loggued_on_user'] = "";
-header("location:log.php");
+header("Location: log.php");
 ?>
